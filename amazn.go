29347c78ecefb721319f031ff80c08e366e3dfeb
Sebastian Döll <sebastian@katallaxie.me>
2020-12-20 21:21:43 +0100

    wip: vendor in

--- amazn.go
+++ amazn.go
@@ -14,18 +14,18 @@ func CreateConfig() *Config {
 	return &Config{}
 }
 
-// Demo ...
-type Demo struct {
+// Amazn ...
+type Amazn struct {
 	name string
 }
 
 // New ...
 func New(config *Config, name string) (iface.ResolverHandler, error) {
-	return &Demo{
+	return &Amazn{
 		name: name,
 	}, nil
 }
 
-func (a *Demo) Resolve(context context.Context, msg []byte) ([]byte, error) {
-	return []byte("demo"), nil
+func (a *Amazn) Resolve(context context.Context, msg []byte) ([]byte, error) {
+	return []byte("amazn"), nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/pluginamzn
 go 1.15
 
 require (
+	github.com/cgentron/api v0.0.0-20201220084924-95323a3b65e6
 	github.com/golang/protobuf v1.4.3
 	google.golang.org/protobuf v1.25.0
 )
