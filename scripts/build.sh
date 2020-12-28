094fe61a71d0b701984618c8d0e58b8c3bd51b11
Sebastian Döll <sebastian@katallaxie.me>
2020-12-28 16:13:53 +0100

    wip: generate specific messages

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,7 @@ wget -qO- https://github.com/cgentron/api/archive/e2c0aa4cdf4264674bf813dcbd2ac9
 protoc --go_out=plugins=grpc:. -I./gen -I. --go_opt=module=github.com/cgentron/pluginamzn cgentron/amazon/annotations.proto
 protoc --go_out=plugins=grpc:. -I./gen -I. --go_opt=module=github.com/cgentron/pluginamzn cgentron/amazon/methods.proto
 protoc --go_out=plugins=grpc:. -I./gen -I. --go_opt=module=github.com/cgentron/pluginamzn cgentron/amazon/fields.proto
+protoc --go_out=plugins=grpc:. -I./gen -I. --go_opt=module=github.com/cgentron/pluginamzn cgentron/amazon/messages.proto
+protoc --go_out=plugins=grpc:. -I./gen -I. --go_opt=module=github.com/cgentron/pluginamzn cgentron/amazon/lambda.proto
 protoc --go_out=plugins=grpc:. -I./gen -I. --go_opt=module=github.com/cgentron/pluginamzn cgentron/amazon/empty.proto
 rm -rf gen
