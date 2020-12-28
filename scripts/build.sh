516ece36ca14726057ae58fda20b347dfb8c7f7c
Sebastian Döll <sebastian@katallaxie.me>
2020-12-28 16:05:45 +0100

    wip: adding message proto

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 mkdir -p gen
-wget -qO- https://github.com/cgentron/api/archive/fcd5132bdeb82ee940cae1c15a606cda2eb13d41.zip | bsdtar --strip-components=1 -C gen -xvf-
+wget -qO- https://github.com/cgentron/api/archive/e2c0aa4cdf4264674bf813dcbd2ac91670c70cb0.zip | bsdtar --strip-components=1 -C gen -xvf-
 protoc --go_out=plugins=grpc:. -I./gen -I. --go_opt=module=github.com/cgentron/pluginamzn cgentron/amazon/annotations.proto
 protoc --go_out=plugins=grpc:. -I./gen -I. --go_opt=module=github.com/cgentron/pluginamzn cgentron/amazon/methods.proto
 protoc --go_out=plugins=grpc:. -I./gen -I. --go_opt=module=github.com/cgentron/pluginamzn cgentron/amazon/fields.proto
